69fb4612c974c398226caef6cfd83f419e65dfe1
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-03-29 22:41:29 -0400

    edit post and delete post working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,26 @@ def create
 	redirect_to posts_path
 end
 
+def show
+	@post = Post.find(params[:id])
+end
+
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	@post = Post.find(params[:id])
+	@post.update(post_params)
+	redirect_to(posts_path(@post))
+end
+
+def destroy
+	@post = Post.find(params[:id])
+	@post.destroy
+	redirect_to posts_path
+end
+
 private
 	 def post_params
 	 	params.require(:post).permit(:image, :caption)
